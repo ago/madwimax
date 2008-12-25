d66229bb8f97fc4ebb2dcba89036562edd48ac5e
Alexander Gordeev <lasaine@lvk.cs.msu.su>
2008-12-25 19:59:35 +0000

    Fix tun_dev.c copyright notice.
    git-svn-id: http://madwimax.googlecode.com/svn/trunk@61 195ec174-b24d-11dd-8bee-0be8dff76351

--- tun_dev.c
+++ tun_dev.c
@@ -1,23 +1,21 @@
 /*
-    VTun - Virtual Tunnel over TCP/IP network.
-
-    Copyright (C) 1998-2008  Maxim Krasnyansky <max_mk@yahoo.com>
-
-    VTun has been derived from VPPP package by Maxim Krasnyansky.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
- */
-
-/*
- * $Id: tun_dev.c,v 1.4.2.1 2008/01/07 22:36:22 mtbishop Exp $
+ * This is a proof-of-concept driver for Samsung SWC-U200 wimax dongle.
+ * Parts of the code in this file are taken from various GPL'd software.
+ * Copyright (C) 2008 Alexander Gordeev <lasaine@lvk.cs.msu.su>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #define HAVE_LINUX_IF_TUN_H
